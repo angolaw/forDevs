0510c8e80b96cde86d869f8b137def5a19c2b54b
Willian S <wsapcf@gmail.com>
2021-04-13 17:54:41 -0300

    ensure HttpAdapter returns data with status code 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:faker/faker.dart';
+import 'package:fordev/data/http/http.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
@@ -8,18 +9,20 @@ import 'package:meta/meta.dart';
 
 class ClientSpy extends Mock implements Client {}
 
-class HttpAdapter {
+class HttpAdapter implements HttpClient {
   final Client client;
 
   HttpAdapter({this.client});
-  Future<void> request(
+  Future<Map> request(
       {@required String url, @required String method, Map body}) async {
     final headers = {
       "content-type": "application/json",
       "accept": "application/json"
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
-    await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    final response =
+        await client.post(Uri.parse(url), headers: headers, body: jsonBody);
+    return jsonDecode(response.body);
   }
 }
 
@@ -27,6 +30,9 @@ void main() {
   Client client;
   HttpAdapter sut;
   String url;
+  void setUpBody() => when(client.post(any,
+          body: anyNamed("body"), headers: anyNamed("headers")))
+      .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
   setUp(() {
     client = ClientSpy();
     sut = HttpAdapter(client: client);
@@ -35,6 +41,7 @@ void main() {
 
   group("post", () {
     test("should call post with the correct values", () async {
+      setUpBody();
       await sut.request(url: url, method: 'post');
       verify(client.post(Uri.parse(url), headers: {
         "content-type": "application/json",
@@ -42,6 +49,7 @@ void main() {
       }));
     });
     test("should have correct JSON headers", () async {
+      setUpBody();
       await sut.request(url: url, method: 'post');
       verify(client.post(Uri.parse(url), headers: {
         "content-type": "application/json",
@@ -49,6 +57,7 @@ void main() {
       }));
     });
     test("should have correct body", () async {
+      setUpBody();
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
       verify(
@@ -63,6 +72,7 @@ void main() {
       );
     });
     test("should call post without body", () async {
+      setUpBody();
       await sut.request(
         url: url,
         method: 'post',
@@ -73,6 +83,13 @@ void main() {
           headers: anyNamed("headers"),
         ),
       );
+    });
+  });
+  group("success cases ", () {
+    test("should return data if post returns 200", () async {
+      setUpBody();
+      final response = await sut.request(url: url, method: 'post');
+      expect(response, {'any_key': 'any_value'});
     });
   });
 }
