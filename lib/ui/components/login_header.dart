a49043dca1e5de280d8393c8b3884203d2398f56
Willian S <wsapcf@gmail.com>
2021-04-13 20:22:30 -0300

    fix yaml identation

--- lib/ui/components/login_header.dart
+++ lib/ui/components/login_header.dart
@@ -8,29 +8,26 @@ class LoginHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 240,
-      margin: EdgeInsets.only(bottom: 32),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Theme.of(context).primaryColorLight,
-                Theme.of(context).primaryColorDark
-              ]),
-          boxShadow: [
-            BoxShadow(
-                offset: Offset(0, 0),
-                spreadRadius: 0,
-                blurRadius: 4,
-                color: Colors.black),
-          ],
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(80),
-          )),
-      child: Image(
-        image: AssetImage('lib/ui/assets/logo.png'),
-      ),
-    );
+        height: 240,
+        margin: EdgeInsets.only(bottom: 32),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Theme.of(context).primaryColorLight,
+                  Theme.of(context).primaryColorDark
+                ]),
+            boxShadow: [
+              BoxShadow(
+                  offset: Offset(0, 0),
+                  spreadRadius: 0,
+                  blurRadius: 4,
+                  color: Colors.black),
+            ],
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(80),
+            )),
+        child: Image.asset('lib/ui/assets/logo.png'));
   }
 }
