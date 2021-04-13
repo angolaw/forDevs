7543c1a1ca41da0bb7e7f38fbc8b655ee51b6218
Willian S <wsapcf@gmail.com>
2021-04-13 17:26:07 -0300

    ensure HttpAdapter calls post with correct body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:faker/faker.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
@@ -16,28 +18,61 @@ class HttpAdapter {
       "content-type": "application/json",
       "accept": "application/json"
     };
-    await client.post(Uri.parse(url), headers: headers);
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(Uri.parse(url), headers: headers, body: jsonBody);
   }
 }
 
 void main() {
+  Client client;
+  HttpAdapter sut;
+  String url;
+  setUp(() {
+    client = ClientSpy();
+    sut = HttpAdapter(client: client);
+    url = faker.internet.httpUrl();
+  });
+
   group("post", () {
     test("should call post with the correct values", () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client: client);
-      final url = faker.internet.httpUrl();
-      await sut.request(url: url, method: 'post');
-      verify(client.post(Uri.parse(url)));
-    });
-    test("should have correct JSON headers", () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client: client);
-      final url = faker.internet.httpUrl();
       await sut.request(url: url, method: 'post');
       verify(client.post(Uri.parse(url), headers: {
         "content-type": "application/json",
         "accept": "application/json"
       }));
+    });
+    test("should have correct JSON headers", () async {
+      await sut.request(url: url, method: 'post');
+      verify(client.post(Uri.parse(url), headers: {
+        "content-type": "application/json",
+        "accept": "application/json"
+      }));
+    });
+    test("should have correct body", () async {
+      await sut
+          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      verify(
+        client.post(
+          Uri.parse(url),
+          headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+          },
+          body: '{"any_key":"any_value"}',
+        ),
+      );
+    });
+    test("should call post without body", () async {
+      await sut.request(
+        url: url,
+        method: 'post',
+      );
+      verify(
+        client.post(
+          any,
+          headers: anyNamed("headers"),
+        ),
+      );
     });
   });
 }
