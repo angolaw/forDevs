3655a7795e70a5b929d9981456cb2f888127c4b4
Willian S <wsapcf@gmail.com>
2021-04-13 08:14:10 -0300

    ensure RemoteAuthentication calls HttpClient with correct method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -9,23 +9,33 @@ class RemoteAuthentication {
 
   RemoteAuthentication({@required this.httpClient, @required this.url});
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(url: url, method: 'post');
   }
 }
 
 abstract class HttpClient {
-  Future<void> request({@required String url});
+  Future<void> request({@required String url, @required String method});
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  test("Should call httpClient with correct url", () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
-    await sut.auth();
+  group("HttpClient", () {
+    test("Should call httpClient with correct url", () async {
+      final httpClient = HttpClientSpy();
+      final url = faker.internet.httpUrl();
+      final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+      await sut.auth();
 
-    verify(httpClient.request(url: url));
+      verify(httpClient.request(url: url, method: 'post'));
+    });
+    test("Should call httpClient with correct values", () async {
+      final httpClient = HttpClientSpy();
+      final url = faker.internet.httpUrl();
+      final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+      await sut.auth();
+
+      verify(httpClient.request(url: url, method: 'post'));
+    });
   });
 }
