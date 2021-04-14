f231ac6905ece3c11fe3a8a6a2fae03cc44de35c
Willian S <wsapcf@gmail.com>
2021-04-14 20:30:44 -0300

    ensure StreamLoginPresenter notifies emailErroStream when a error ocurr

--- test/ui/presentation/presenters/stream_login_presenter_test.dart
+++ test/ui/presentation/presenters/stream_login_presenter_test.dart
@@ -1,14 +1,26 @@
+import 'dart:async';
+
 import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+class LoginState {
+  String emailError;
+}
+
 class StreamLoginPresenter {
   final Validation validation;
-
   StreamLoginPresenter({@required this.validation});
+
+  final _controller = StreamController<LoginState>.broadcast();
+  var _state = LoginState();
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
+
   void validateEmail(String email) {
-    validation.validate(field: 'email', value: email);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -31,5 +43,15 @@ void main() {
   test("should call validation with correct email", () {
     sut.validateEmail(email);
     verify(validation.validate(field: "email", value: email)).called(1);
+  });
+
+  test("should emit email error if validation fails", () {
+    when(validation.validate(
+            field: anyNamed('field'), value: anyNamed('value')))
+        .thenReturn('error');
+
+    expectLater(sut.emailErrorStream, emits('error'));
+
+    sut.validateEmail(email);
   });
 }
