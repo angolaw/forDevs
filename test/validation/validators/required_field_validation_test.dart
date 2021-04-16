8aa58f08d321f81a7c6640a6662e1959092da7c8
Willian S <wsapcf@gmail.com>
2021-04-16 08:16:16 -0300

    ensure RequiredFieldValidation return Campo obrigatorio if value is empty

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -11,15 +11,21 @@ class RequiredFieldValidation implements FieldValidation {
   RequiredFieldValidation(this.field);
 
   String validate(String value) {
-    return null;
+    return value.isEmpty ? "Campo obrigatorio" : null;
   }
 }
 
 void main() {
-  test("should return null  if value is not empty", () {
+  test("should return null if value is not empty", () {
     final sut = RequiredFieldValidation('any_field');
     final error = sut.validate('any_value');
 
     expect(error, null);
+  });
+  test("should return error if value is empty", () {
+    final sut = RequiredFieldValidation('any_field');
+    final error = sut.validate('');
+
+    expect(error, "Campo obrigatorio");
   });
 }
