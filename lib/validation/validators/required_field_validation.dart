d9689d58d9ef8ef92e6541aa12b27c8e80e8073f
Willian S <wsapcf@gmail.com>
2021-04-16 08:26:13 -0300

    move RequiredFieldValidation to its own file

---
+++ lib/validation/validators/required_field_validation.dart
@@ -0,0 +1,11 @@
+import 'package:fordev/validation/protocols/protocols.dart';
+
+class RequiredFieldValidation implements FieldValidation {
+  final String field;
+
+  RequiredFieldValidation(this.field);
+
+  String validate(String value) {
+    return value?.isNotEmpty == true ? null : "Campo obrigatorio";
+  }
+}
