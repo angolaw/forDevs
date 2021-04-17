3370cd58c1048c8b55ed3b92d7e6babb9825e85f
Willian S <wsapcf@gmail.com>
2021-04-17 10:02:08 -0300

    ensure makeLoginPage calls makeLoginPresenter on presenter parameter

--- lib/main/factories/pages/login/login_page_factory.dart
+++ lib/main/factories/pages/login/login_page_factory.dart
@@ -1,25 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:fordev/data/usecases/usecases.dart';
-import 'package:fordev/infra/http/http.dart';
-import 'package:fordev/presentation/presenter/presenter.dart';
 import 'package:fordev/ui/pages/pages.dart';
-import 'package:fordev/validation/validators/validators.dart';
-import 'package:http/http.dart';
+
+import '../../factories.dart';
 
 Widget makeLoginPage() {
-  final client = Client();
-  final httpAdapter = HttpAdapter(client: client);
-  final url = 'http://fordevs.herokuapp.com/api/login';
-  final remoteAuthentication =
-      RemoteAuthentication(httpClient: httpAdapter, url: url);
-  final validationComposite = ValidationComposite(validations: [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
-  ]);
-  final streamLoginPresenter = StreamLoginPresenter(
-      authentication: remoteAuthentication, validation: validationComposite);
   return LoginPage(
-    presenter: streamLoginPresenter,
+    presenter: makeLoginPresenter(),
   );
 }
