4f96e5331f1535d27cb68ee70a0ee8b6bc4e67a3
Willian S <wsapcf@gmail.com>
2021-04-17 10:26:54 -0300

    add ValidationBuilder with Builder pattern

--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,3 +1,4 @@
+import 'package:fordev/main/builders/builders.dart';
 import 'package:fordev/presentation/protocols/protocols.dart';
 import 'package:fordev/validation/protocols/protocols.dart';
 import 'package:fordev/validation/validators/validators.dart';
@@ -8,8 +9,7 @@ Validation makeLoginValidation() {
 
 List<FieldValidation> makeLoginValidations() {
   return [
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build()
   ];
 }
