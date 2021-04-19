61cc8262116d64e0a3925166ee348fd7c3b8f904
Willian S <wsapcf@gmail.com>
2021-04-19 15:41:04 -0300

    ensure SplashPage calls presenter.loadCurrentAccount on start

--- requirements/checklists/pages/splash_page.md
+++ requirements/checklists/pages/splash_page.md
@@ -2,6 +2,6 @@
 
 ## Regras
 1. ✅ Mostrar o loading ao iniciar a página
-2. Chamar o carregador de dados da conta atual
+2. ✅ Chamar o carregador de dados da conta atual
 3. Navegar para a página correta no final do carregamento
 4. Fechar streams quando a página for encerrada
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
 
 class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({Key key, @required this.presenter}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
     return Scaffold(
       appBar: AppBar(title: Text("4Dev")),
       body: Center(
@@ -14,13 +21,32 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
 void main() {
-  testWidgets("should presente loading on page load",
-      (WidgetTester tester) async {
+  SplashPresenterSpy presenter;
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashPresenterSpy();
     await tester.pumpWidget(GetMaterialApp(
       initialRoute: '/',
-      getPages: [GetPage(name: "/", page: () => SplashPage())],
+      getPages: [
+        GetPage(name: "/", page: () => SplashPage(presenter: presenter))
+      ],
     ));
+  }
+
+  testWidgets("should present loading on page load",
+      (WidgetTester tester) async {
+    await loadPage(tester);
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+  testWidgets("should call loadCurrentAccount on page load",
+      (WidgetTester tester) async {
+    await loadPage(tester);
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }
