79b8d83a2a2aebacc39e07632a003a9927a02927
Willian S <wsapcf@gmail.com>
2021-04-19 16:00:01 -0300

    move SplashPage content to production code

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'splash_presenter.dart';
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  const SplashPage({Key key, @required this.presenter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("4Dev"),
+      ),
+      body: Builder(
+        builder: (context) {
+          presenter.navigateToStream.listen(
+            (page) {
+              if (page?.isNotEmpty == true) {
+                Get.offAllNamed(page);
+              }
+            },
+          );
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+}
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -2,39 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/ui/pages/splash/splash.dart';
 import 'package:get/get.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
-
-class SplashPage extends StatelessWidget {
-  final SplashPresenter presenter;
-
-  const SplashPage({Key key, @required this.presenter}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    presenter.loadCurrentAccount();
-    return Scaffold(
-        appBar: AppBar(title: Text("4Dev")),
-        body: Builder(
-          builder: (context) {
-            presenter.navigateToStream.listen((page) {
-              if (page?.isNotEmpty == true) {
-                Get.offAllNamed(page);
-              }
-            });
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ));
-  }
-}
-
-abstract class SplashPresenter {
-  Stream<String> get navigateToStream;
-  Future<void> loadCurrentAccount();
-}
 
 class SplashPresenterSpy extends Mock implements SplashPresenter {}
 
