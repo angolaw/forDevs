4a4beb2442aa4dd5cfb8c7f13cd5744c4be8d711
Willian S <wsapcf@gmail.com>
2021-04-20 13:16:36 -0300

    move SUT creation to the setup method

--- test/ui/presentation/presenters/getx_splash_presenter_test.dart
+++ test/ui/presentation/presenters/getx_splash_presenter_test.dart
@@ -22,9 +22,19 @@ class GetxSplashPresenter implements SplashPresenter {
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
 void main() {
+  LoadCurrentAccount loadCurrentAccount;
+  GetxSplashPresenter sut;
+  setUp(() {
+    loadCurrentAccount = LoadCurrentAccountSpy();
+    sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+  });
   test('should call LoadCurrentAccount ', () async {
-    final loadCurrentAccount = LoadCurrentAccountSpy();
-    final sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+    //quando eu chamar o metodo checkAccount
+    await sut.checkAccount();
+    //faca com que ele chame o loadCurrentAccount.load()
+    verify(loadCurrentAccount.load()).called(1);
+  });
+  test('should call LoadCurrentAccount ', () async {
     //quando eu chamar o metodo checkAccount
     await sut.checkAccount();
     //faca com que ele chame o loadCurrentAccount.load()
