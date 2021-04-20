8051af594b4cb9f5d71b3968442471e7f3a634f2
Willian S <wsapcf@gmail.com>
2021-04-20 13:40:15 -0300

    move production code to their own files

---
+++ lib/presentation/presenter/getx_splash_presenter.dart
@@ -0,0 +1,23 @@
+import 'package:fordev/domain/usecases/usecases.dart';
+import 'package:fordev/ui/pages/pages.dart';
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+
+class GetxSplashPresenter implements SplashPresenter {
+  final LoadCurrentAccount loadCurrentAccount;
+
+  var _navigateTo = RxString('');
+
+  GetxSplashPresenter({@required this.loadCurrentAccount});
+  Stream<String> get navigateToStream => _navigateTo.stream;
+
+  @override
+  Future<void> checkAccount() async {
+    try {
+      final account = await loadCurrentAccount.load();
+      _navigateTo.value = account == null ? '/login' : '/surveys';
+    } catch (error) {
+      _navigateTo.value = '/login';
+    }
+  }
+}
--- test/ui/presentation/presenters/getx_splash_presenter_test.dart
+++ test/ui/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,32 +1,9 @@
 import 'package:faker/faker.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:fordev/domain/entities/entities.dart';
-import 'package:fordev/domain/helpers/helpers.dart';
 import 'package:fordev/domain/usecases/load_current_account.dart';
-import 'package:fordev/ui/pages/pages.dart';
-import 'package:get/get.dart';
-import 'package:meta/meta.dart';
+import 'package:fordev/presentation/presenter/presenter.dart';
 import 'package:mockito/mockito.dart';
-
-class GetxSplashPresenter implements SplashPresenter {
-  final LoadCurrentAccount loadCurrentAccount;
-
-  var _navigateTo = RxString('');
-
-  GetxSplashPresenter({@required this.loadCurrentAccount});
-  Stream<String> get navigateToStream => _navigateTo.stream;
-
-  @override
-  Future<void> checkAccount() async {
-    try {
-      final account = await loadCurrentAccount.load();
-      _navigateTo.value = account == null ? '/login' : '/surveys';
-    } catch (error) {
-      _navigateTo.value = '/login';
-    }
-  }
-}
 
 class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 
