6167e49deeccef2e89e956dbcd46bce5e181264f
Willian S <wsapcf@gmail.com>
2021-04-20 13:28:22 -0300

    ensure GetxLoginPresenter returns /login route when account data is null

--- test/ui/presentation/presenters/getx_splash_presenter_test.dart
+++ test/ui/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,4 +1,6 @@
+import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:fordev/domain/entities/entities.dart';
 import 'package:fordev/domain/usecases/load_current_account.dart';
 import 'package:fordev/ui/pages/pages.dart';
 import 'package:get/get.dart';
@@ -15,8 +17,11 @@ class GetxSplashPresenter implements SplashPresenter {
 
   @override
   Future<void> checkAccount() async {
-    _navigateTo.value = '/surveys';
-    await loadCurrentAccount.load();
+    final account = await loadCurrentAccount.load();
+    if (account?.isBlank == false) {
+      _navigateTo.value = '/surveys';
+    }
+    _navigateTo.value = '/login';
   }
 }
 
@@ -25,9 +30,16 @@ class LoadCurrentAccountSpy extends Mock implements LoadCurrentAccount {}
 void main() {
   LoadCurrentAccount loadCurrentAccount;
   GetxSplashPresenter sut;
+  String token;
+  void mockLoadCurrentAccount({AccountEntity account}) {
+    when(loadCurrentAccount.load()).thenAnswer((_) async => account);
+  }
+
   setUp(() {
+    token = faker.guid.guid();
     loadCurrentAccount = LoadCurrentAccountSpy();
     sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+    mockLoadCurrentAccount(account: AccountEntity(token: token));
   });
   test('should call LoadCurrentAccount ', () async {
     //quando eu chamar o metodo checkAccount
@@ -35,9 +47,14 @@ void main() {
     //faca com que ele chame o loadCurrentAccount.load()
     verify(loadCurrentAccount.load()).called(1);
   });
-  test('should go to surveys page in success', () async {
+  test('should go to surveys page on success', () async {
     sut.navigateToStream
         .listen(expectAsync1((page) => expect(page, '/surveys')));
+    await sut.checkAccount();
+  });
+  test('should go to login if cache data is null', () async {
+    mockLoadCurrentAccount(account: null);
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
     await sut.checkAccount();
   });
 }
