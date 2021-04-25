f5daee64bfcde3a5f11403dac4ebfdd2d2815232
Willian S <wsapcf@gmail.com>
2021-04-24 21:11:41 -0300

    refactor required_field_validation_test

--- test/validation/validators/required_field_validation_test.dart
+++ test/validation/validators/required_field_validation_test.dart
@@ -9,16 +9,16 @@ void main() {
   });
 
   test("should return null if value is not empty", () {
-    final error = sut.validate('any_value');
+    final error = sut.validate({'any_field': 'any_value'});
 
     expect(error, null);
   });
   test("should return error if value is empty", () {
-    final error = sut.validate('');
+    final error = sut.validate({'any_field': ''});
     expect(error, ValidationError.requiredField);
   });
   test("should return error if value is null", () {
-    final error = sut.validate(null);
+    final error = sut.validate({'any_field': null});
     expect(error, ValidationError.requiredField);
   });
 }
