6a33d25e9e17132ebb05a07baee3b146d77c30be
Willian S <wsapcf@gmail.com>
2021-04-26 10:53:28 -0300

    refactor on page tests to remove intial state testing

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -81,30 +81,6 @@ void main() {
   });
 
   group("loginPage", () {
-    testWidgets("Should load with initial state correct",
-        (WidgetTester tester) async {
-      //arrange
-      await loadPage(tester);
-
-      final emailTextChildren = find.descendant(
-          of: find.bySemanticsLabel("Email"), matching: find.byType(Text));
-
-      expect(emailTextChildren, findsOneWidget,
-          reason:
-              'when a textformfield has only one child, means it has no error, since this child is the hint text');
-
-      final passwordTextChildren = find.descendant(
-          of: find.bySemanticsLabel("Senha"), matching: find.byType(Text));
-
-      expect(passwordTextChildren, findsOneWidget,
-          reason:
-              'when a textformfield has only one child, means it has no error, since this child is the hint text');
-
-      final button = tester.widget<RaisedButton>(find.byType(RaisedButton));
-
-      expect(button.onPressed, null);
-    });
-
     //validate form
     testWidgets("should call validate with correct values",
         (WidgetTester tester) async {
