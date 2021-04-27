718e2580da2807f0c009606df598d89fbe7c93f4
Willian S <wsapcf@gmail.com>
2021-04-27 19:40:23 -0300

    ensure decorator adds new header reuses header if it contains something

--- test/main/decorators/authorize_http_client_decorator_test.dart
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -18,7 +18,8 @@ class AuthorizeHttpClientDecorator {
       Map body,
       Map headers}) async {
     final token = await fetchSecureCacheStorage.fetchSecure('token');
-    final authorizedHeaders = {'x-access-token': token};
+    final authorizedHeaders = headers ?? {}
+      ..addAll({'x-access-token': token});
     await decoratee.request(
         url: url, method: method, body: body, headers: authorizedHeaders);
   }
@@ -72,6 +73,21 @@ void main() {
         method: method,
         body: body,
         headers: {'x-access-token': token},
+      ),
+    ).called(1);
+
+    await sut.request(
+      url: url,
+      method: method,
+      body: body,
+      headers: {'any_header': 'any_value'},
+    );
+    verify(
+      httpClient.request(
+        url: url,
+        method: method,
+        body: body,
+        headers: {'x-access-token': token, 'any_header': 'any_value'},
       ),
     ).called(1);
   });
